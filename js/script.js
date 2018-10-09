3b3ace8bbb49bc5a84ebc4b79c7434abefb5a1b8
Vitalik <ztx25@meta.ua>
2018-10-09 17:01:30 +0300

    script file

---
+++ js/script.js
@@ -0,0 +1,55 @@
+	$(document).ready(function(){
+	        PopUpHide();
+	});
+	
+	 function PopUpShow(){
+	        $("#popup1").show();
+	        let date = $("#date").text();
+	        $("#dateShow").text(date);
+	}
+
+	 function PopUpShow1(){
+	        $("#popup1").show();
+	        let date = $("#date1").text();
+	        $("#dateShow").text(date);
+	}
+	function PopUpShow2(){
+	        $("#popup1").show();
+	        let date = $("#date2").text();
+	        $("#dateShow").text(date);
+	}
+	function PopUpShow3(){
+	        $("#popup1").show();
+	        let date = $("#date3").text();
+	        $("#dateShow").text(date);
+	}
+	function PopUpShow4(){
+	        $("#popup1").show();
+	        let date = $("#date4").text();
+	        $("#dateShow").text(date);
+	}
+	function PopUpShow5(){
+	        $("#popup1").show();
+	        let date = $("#date5").text();
+	        $("#dateShow").text(date);
+	}
+	function PopUpShow6(){
+	        $("#popup1").show();
+	        let date = $("#date6").text();
+	        $("#dateShow").text(date);
+	}
+	function PopUpShow7(){
+	        $("#popup1").show();
+	        let date = $("#date7").text();
+	        $("#dateShow").text(date);
+	}
+	
+
+
+	function PopUpHide(){
+	        $("#popup1").hide();
+	}
+
+
+
+
